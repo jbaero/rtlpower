1ceef78c6642677fdb5cddff61d4f282ebdf2e77
jbaero <143622575+jbaero@users.noreply.github.com>
2023-08-30 16:15:45 +0200

    Update rtlpower.js

--- rtlpower.js
+++ rtlpower.js
@@ -7,28 +7,58 @@ module.exports = function(RED) {
         var node = this;
 
         node.on('input', function(msg) {
-            // Récupération des paramètres à partir du message d'entrée
-            var low = msg.low || "default_low_value";
-            var high = msg.high || "default_high_value";
-            var bw = msg.bw || "default_bw_value";
-            var time = msg.time || 10;
-            var gain = msg.gain || "default_gain_value";
+            var low = msg.payload.low || "default_low_value";
+            var high = msg.payload.high || "default_high_value";
+            var bw = msg.payload.bw || "default_bw_value";
+            var time = msg.payload.time || 10;
+            var gain = msg.payload.gain || "default_gain_value";
 
             var cmd = "rtl_power";
             var args = ["-i", time, "-g", gain, "-f", low + ":" + high + ":" + bw];
 
             try {
                 var child = spawn(cmd, args);
-                
-                // Le reste du code reste le même
-                
+
+                child.stdout.on('data', function (data) {
+                    // Même logique que dans le code original pour gérer la sortie standard
+                });
+
+                child.stderr.on('data', function (data) {
+                    // Même logique que dans le code original pour gérer les erreurs
+                });
+
+                child.on('close', function (code, signal) {
+                    // Même logique que dans le code original pour gérer la fermeture du processus
+                });
+
+                child.on('error', function (err) {
+                    // Même logique que dans le code original pour gérer les erreurs
+                });
+
             } catch(err) {
-                // Gestion des erreurs
+                // Gestion des erreurs, par exemple :
+                node.error(err);
+                node.status({fill:"red",shape:"ring",text:"error"});
             }
         });
 
-        // ... (la gestion des événements 'close', 'stdout', 'stderr' peut rester la même)
+        node.on("close", function(done) {
+            if (node.child != null) {
+                var tout = setTimeout(function() {
+                    node.child.kill("SIGKILL");
+                    done();
+                }, 3000);
+                node.child.on('exit', function() {
+                    if (tout) { clearTimeout(tout); }
+                    done();
+                });
+                if (RED.settings.verbose) { node.log("rtl_power stopped"); }
+            } else { 
+                setTimeout(function() { done(); }, 100); 
+            }
+            node.status({});
+        });
     }
 
     RED.nodes.registerType("rtlpower", RtlPowerNode);
-}
+};
